7ed2cbc06700c7beaaa06ee4e1a1d87d1b0afbb4
Letsoken <lettyisoken@gmail.com>
2022-10-15 05:57:49 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,12 +3,20 @@
 /* Alphabets */
 
 /**
-  * main - Display alphabets in lower letter
+  * main - Display alphabets in lower letter except a and q
   *
-  * Description: displays only lower case letters with a new line
+  * Description: displays only lower case letters except q and e
   * Return: Always equals 0
   */
 int main(void)
 {
+	int c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+		if (c != 'q' || c != 'e')
+			putchar(c);
+
+	putchar('\n');
+
 	return (0);
 }
