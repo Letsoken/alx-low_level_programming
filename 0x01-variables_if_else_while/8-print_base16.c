5034d4ca08d54810580e2983451ae0ee0a777fd9
Letsoken <lettyisoken@gmail.com>
2022-10-15 16:21:53 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
-/* Alphabets */
+/* Hex Numbers */
 
 /**
-  * main - Display alphabets in lower letter
+  * main - Display numbers in base 16
   *
-  * Description: displays only lower case letters with a new line
+  * Description: displays numbers 0 to f in base 16
   * Return: Always equals 0
   */
 int main(void)
 {
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+		putchar((n % 10) + '0');
+	for (n = 'a' ; n <= 'f'; n++)
+		putchar(n);
+
+	printf("\n");
+
 	return (0);
 }
