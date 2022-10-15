e165ac6f14f28f63aab76621f430d7c6d36654c9
Letsoken <lettyisoken@gmail.com>
2022-10-15 05:00:47 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,12 +3,21 @@
 /* Alphabets */
 
 /**
-  * main - Display alphabets in lower letter
+  * main - Display alphabets in lower letter and upper letters
   *
-  * Description: displays only lower case letters with a new line
+  * Description: displays lower and upper case letters
   * Return: Always equals 0
   */
 int main(void)
 {
+	int c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+		putchar(c);
+
+	for (c = 'A' ; c <= 'Z' ; c++)
+		putchar(c);
+
+	putchar('\n');
 	return (0);
 }
