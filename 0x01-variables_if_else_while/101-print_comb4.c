92b22ec299480f8e4ceca0b9a8165a3b3d0b1b27
Letsoken <lettyisoken@gmail.com>
2022-10-16 01:01:19 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,14 +1,40 @@
 #include <stdio.h>
 
-/* Alphabets */
+/* Numbers */
 
 /**
-  * main - Display alphabets in lower letter
+  * main - Display all possible different combinations of 3 digits
   *
-  * Description: displays only lower case letters with a new line
+  * Description: displays numbersall possible different combination
+  * of 3 digits separated by commas
   * Return: Always equals 0
   */
 int main(void)
 {
+	int i;
+	int j;
+	int k;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 1 ; j < 10 ; j++)
+		{
+			for (k = 2 ; k < 10 ; k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar((i % 10) + '0');
+					putchar((j % 10) + '0');
+					putchar((k % 10) + '0');
+					if (i == 7 && j == 8 && k == 9)
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
 	return (0);
 }
