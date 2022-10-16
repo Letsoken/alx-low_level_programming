68429c54a9eced4b5811d4af44611f8bd65c8302
Letsoken <lettyisoken@gmail.com>
2022-10-15 17:53:56 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,14 +1,35 @@
 #include <stdio.h>
 
-/* Alphabets */
+/* Numbers */
 
 /**
-  * main - Display alphabets in lower letter
+  * main - Display all possible different combinations of two digits
   *
-  * Description: displays only lower case letters with a new line
+  * Description: displays numbersall possible different combination
+  * of two digits separated by commas
   * Return: Always equals 0
   */
 int main(void)
 {
+	int i;
+	int j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 1 ; j < 10 ; j++)
+		{
+			if (i < j)
+			{
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				if (i == 8 && j == 9)
+					break;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
 	return (0);
 }
