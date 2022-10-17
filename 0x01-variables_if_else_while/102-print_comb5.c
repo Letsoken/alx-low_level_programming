6379ffc677c7cce92b6a006a52aa4deb5b0bba01
Letsoken <lettyisoken@gmail.com>
2022-10-17 01:26:04 -0700

    ./102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
 
-/* Alphabets */
+/* Numbers */
 
 /**
-  * main - Display alphabets in lower letter
+  * main - Display all possible different combinations of
+  * 2 two digit numbers
   *
-  * Description: displays only lower case letters with a new line
+  * Description: displays all possible different combination
+  * of 2 two digits separated by commas
   * Return: Always equals 0
   */
 int main(void)
 {
+	int i;
+	int j;
+
+	for (i = 0 ; i < 99 ; i++)
+	{
+		for (j = i + 1 ; j < 100 ; j++)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i == 98 && j == 99)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
 	return (0);
 }
