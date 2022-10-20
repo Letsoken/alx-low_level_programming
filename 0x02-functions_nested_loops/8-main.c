87ad5e77fa5cfd7e61d7ce50609ef25392f9ebc1
Letsoken <lettyisoken@gmail.com>
2022-10-20 01:18:47 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Prints all the minutes in 24 hrs
+ *
+ * Description: print minutee
+ * Return: Always equals 0
+ */
+int main(void)
+
+{
+	jack_bauer();
+
+	return (0);
+
+}
