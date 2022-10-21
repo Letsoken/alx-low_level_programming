f8e506cca5eaf20036a4196ca76ad6b5ee99fbd4
Letsoken <lettyisoken@gmail.com>
2022-10-20 20:18:36 -0700

    ./3-main_b

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -2,8 +2,8 @@
 #include "main.h"
 
 /**
-  * main - takes a date and prints how many days are left in the year,
-  * taking leap years into account
+  * main - takes a date and prints how many days are left in the year, taking
+  * leap years into account
   * Return: 0
   */
 
