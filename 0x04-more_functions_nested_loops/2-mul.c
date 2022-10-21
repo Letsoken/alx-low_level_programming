c207d8818d7b86322d94e633f61af0769c278557
Letsoken <lettyisoken@gmail.com>
2022-10-21 16:02:43 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 numbers
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Description: Multiplies 2 numbers
+ * Return: Always returns product
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+
+}
+
