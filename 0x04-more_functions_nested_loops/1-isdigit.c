ba6b89b00019f94f8a50f424659cda2fa0f93082
Letsoken <lettyisoken@gmail.com>
2022-10-21 03:48:18 -0700

    Add Readme

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - Identifies digits
+ * @c: First operand
+ *
+ * Description: Determines if character is digit
+ * Return: Always equals 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >=0'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+
+}
+
