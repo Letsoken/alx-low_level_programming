dc18805fbcb66d9b188b28d407bebe1bb6c3d35a
Letsoken <lettyisoken@gmail.com>
2022-10-21 21:37:51 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - print integers
+ * @n : First operand
+ *
+ * Description: prints integers
+ *
+ */
+
+void print_number(int n)
+{
+	unsigned int number = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		number = -number;
+	}
+	if ((number / 10) > 0)
+		print_number(number / 10);
+
+	_putchar((number % 10) + '0');
+
+}
+
