466a6cb104b032b5dbd6690e1651b1e6263d6910
Letsoken <lettyisoken@gmail.com>
2022-10-21 19:11:17 -0700

    8-squares.c

---
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Description: Main
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_square(2);
+	print_square(10);
+	print_square(0);
+
+
+	return (0);
+}
