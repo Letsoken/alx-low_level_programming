a06042e2dc0730a1032722ea0af396be8a7b9d95
Letsoken <lettyisoken@gmail.com>
2022-10-26 06:03:12 +0000

    9-strcpy. Adjust for null pointer

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,16 +4,18 @@
   * *_strcpy - displays n elements of an array
   * @dest : pointer to destination
   * @src : pointer to source
-  * Return: nothing
+  * Return: pointer to destination
   */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	char *ptr = dest;
 
-	while (src[i])
+	while (*src != '\0')
 	{
-		dest[i] = src[i];
-		i++;
+		*dest = *src;
+		dest++;
+		src++;
 	}
-	return (dest);
+	*dest = '\0';
+	return (ptr);
 }
