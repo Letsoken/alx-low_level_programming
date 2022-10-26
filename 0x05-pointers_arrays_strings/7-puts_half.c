0e25c63cd9bd78a4259c4ef3b24d960a2961b029
Letsoken <lettyisoken@gmail.com>
2022-10-26 02:52:17 +0000

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * puts_half - displays every other string
+  * @str : pointer to an char
+  * Return: nothing
+  */
+void puts_half(char *str)
+{
+	int i = 0;
+	int l = 0;
+	int n;
+
+	while (str[i++])
+		l++;
+	if ((l % 2) == 0)
+		n = l / 2;
+	else
+		n = (l - 1) / 2;
+
+	for (i = n ; i < l ; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
