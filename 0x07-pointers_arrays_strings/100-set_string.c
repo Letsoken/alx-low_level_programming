24010cb34918ffbd670b9090a94916b08ae06137
Letsoken <lettyisoken@gmail.com>
2022-10-31 22:44:36 +0000

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * set_string - set value of pointer to a sting
+  *
+  * @s: Pointer to pointer
+  * @to: Pointer
+  * Return: No return
+  */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
