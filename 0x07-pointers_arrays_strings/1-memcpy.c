106b767955be62141050d3410e3708b17a1acf96
Letsoken <lettyisoken@gmail.com>
2022-11-01 05:00:19 +0000

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _memcpy - copies memory area
+  *
+  * @dest: Pointer to destination
+  * @src: pointer to source to be copied
+  * @n: number of elements to be copied
+  * Return: pointer to destination
+  */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+		dest[i] = src[i];
+	return (dest);
+}
