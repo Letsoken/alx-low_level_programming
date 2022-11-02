807419ed8c2c9fe9213593d7b1aef9c6c09d3972
Letsoken <lettyisoken@gmail.com>
2022-11-02 13:50:04 +0000

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,13 @@
   */
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	s++;
-	if (*s != '\0')
-		_puts_recursion(s);
-	
 	if (*s == '\0')
-		 _putchar('\n');
+		_putchar('\n');
+
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
 }
