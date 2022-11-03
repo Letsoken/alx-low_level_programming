035c21d4be9439c0a5005ba6ee54a4754a3e0f72
Letsoken <lettyisoken@gmail.com>
2022-11-03 01:25:29 +0000

    1-print_rev_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,11 @@
   */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-		_putchar('\n');
-
 	if (*s != '\0')
 	{
 		_putchar(*s);
-		s++;
-		_puts_recursion(s);
+		_puts_recursion(s + 1);
 	}
+	else
+		_putchar('\n')
 }
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
-  * _puts_recursion - prints a string, followed by a new line.
+  * _print_rev_recursion - prints a string in reverse.
   *
   * @s: pointer to a string
   * Return: Nothing
   */
-void _puts_recursion(char *s)
+
+void _print_rev_recursion(char *s)
 {
-	_putchar(*s);
-	s++;
-	if (*s != '\0')
+	if (*s)
 	{
-		_puts_recursion(s);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	_putchar('\n');
+	
 }
