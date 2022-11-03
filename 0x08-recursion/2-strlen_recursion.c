7f1dadd214dc63f2db6ee4fbcc0bd6dc5539e856
Letsoken <lettyisoken@gmail.com>
2022-11-03 02:31:43 +0000

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _strlen_recursion - calculates string length
+  *
+  * @s: pointer to a string
+  * Return: string length
+  */
+int _strlen_recursion(char *s)
+{
+	int l = 0;
+	
+	if (*s != '\0')
+	{
+		l++;
+		l = l +_strlen_recursion(s + 1);
+	}
+	return (l);
+}
