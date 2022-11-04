e35a5fb2932d5661e3ab4d585d1b4024cd7ba794
Letsoken <lettyisoken@gmail.com>
2022-11-04 23:37:22 +0000

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,13 @@
 
 /**
   * main - check the code
+  * @argc: argument count
+  * @argv: argument vector or array
   *
-  * 
   * Return: Always 0.
   */
 int main(__attribute__((unused))int argc, char *argv[])
 {
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
