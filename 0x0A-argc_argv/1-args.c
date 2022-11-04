200b6febee0e84a825f51629500118550dd84edb
Letsoken <lettyisoken@gmail.com>
2022-11-04 23:44:17 +0000

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - check the code
+  * @argc: argument count
+  * @argv: argument vector or array
+  *
+  * Return: Always 0.
+  */
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
