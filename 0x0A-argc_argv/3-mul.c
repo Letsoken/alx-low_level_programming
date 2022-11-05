ff023894bd4098ef941e018141c0d9864208dd99
Letsoken <lettyisoken@gmail.com>
2022-11-05 03:29:55 +0000

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,14 +9,16 @@
   */
 int main(int argc, char *argv[])
 {
-	int mul;
+	int mul, n1, n2;
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	mul = atoi(argv[1]) * atoi(argv[2]);
+	n1 = atoi(argv[1]);	
+	n2 = atoi(argv[2]);
+	mul = n1 * n2;
 	printf("%d\n", mul);
 	return (0);
 }
