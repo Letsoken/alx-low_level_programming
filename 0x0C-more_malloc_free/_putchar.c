7e32fed6229a879ca69bf6d5b61b7611e49381fd
Letsoken <lettyisoken@gmail.com>
2022-11-11 04:22:39 +0000

    101-mul

---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _putchar - Print
+ * @c : First operand
+ *
+ * Description: print
+ * Return: Always return 0
+ */
+int _putchar(int c)
+{
+	putchar(c);
+
+	return (0);
+}
