5ac9d849c38a8140009bd8d5f69bf91a7e57f7ce
Letsoken <lettyisoken@gmail.com>
2022-11-15 03:48:17 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  * struct dog - create type dog
+  * @name: First member
+  * @age: Second member
+  * @owner: Third member
+  *
+  * Description: all members are valid properties of dog
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
