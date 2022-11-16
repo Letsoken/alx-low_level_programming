b1671aea7be21f6dc00f544d99aa8ca2fe003437
Letsoken <lettyisoken@gmail.com>
2022-11-16 12:39:47 +0000

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,15 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
   * print_name - Function that prints a name
   * @name: name of the person
-  *
+  * @f: function pointer
   * Return: Nothing.
   */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL)
+		return;
 	f(name);
 }
-
