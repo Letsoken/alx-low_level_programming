dd81c8b433d30ad2ae9c5e829b47a228dbd6f034
Letsoken <lettyisoken@gmail.com>
2022-12-01 02:30:40 +0000

    Tests 1, 2, 3 and 4

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,24 +1,21 @@
 #include "lists.h"
 
 /**
-  * list_len - Prints all the elements of a list_t list.
-  * @h: The list_t list.
-  *
-  * Return: The number of nodes in h.
-  */
-
+ * list_len - Finds the number of elements in
+ *            a linked list_t list.
+ * @h: The linked list_t list.
+ *
+ * Return: The number of elements in h.
+ */
 size_t list_len(const list_t *h)
 {
-	size_t i;
+	size_t elements = 0;
 
-	for (i = 0 ; h ; i++)
+	while (h)
 	{
+		elements++;
 		h = h->next;
 	}
 
-	return (i);
+	return (elements);
 }
-
-
-
-
