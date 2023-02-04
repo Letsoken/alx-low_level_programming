bccc067c60fa0e8732c9e8332d76adc66a9c4568
Letsoken <lettyisoken@gmail.com>
2023-02-04 05:29:26 +0100

    t

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,10 +16,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	table->array = malloc(table->size, sizeof(hash_node_t *));
+	table->array = malloc(table->size * sizeof(hash_node_t *));
 
 	if (table->array == NULL)
-		free(table);
 		return (NULL);
 
 	for (i = 0; i < table->size; i++)
