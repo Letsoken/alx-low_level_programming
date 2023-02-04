08ed11bd45a79bd72d0f3aecf49066fdb56beb89
Letsoken <lettyisoken@gmail.com>
2023-02-04 05:07:22 +0100

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,19 +2,20 @@
 
 /**
  * hash_table_create- Function creates a new hashtable
- * @size: size or capacity of new hashtable
+ * @size: size of new hashtable
+ *
  * Return: Pointer to a newly created hashtable on success
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	hash_table_t *table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
 		return (NULL);
 	
 	table->size = size
-	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
+	table->array = malloc(table->size, sizeof(hash_node_t *));
 	
 	if (table->array == NULL)
 		free(table);
